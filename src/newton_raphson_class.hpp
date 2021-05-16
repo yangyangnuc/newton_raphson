33bcf9f15f40227c51a61613844bcbdfc2df94e0
Your Name <you@example.com>
2021-05-16 12:23:13 +0800

    ing

---
+++ src/newton_raphson_class.hpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+
+
+
+class Newton_raphson
+{
+    public:
+        double eps;
+        unsigned long max_iter;
+
+        Newton_raphson() : eps(0.001), max_iter(10000)
+        {
+            ;
+        } 
+
+}_;
---
+++ src/wrapper.cpp
@@ -0,0 +1,22 @@
+/*
+ * 
+ * newton method, also called newton-raphson method to iterate taylor expansion to find equation's solution
+ * 
+ */
+
+
+
+#include <iostream>
+
+
+
+int main(int argc, char** argv)
+{
+
+
+
+
+
+
+    return 0;
+}
