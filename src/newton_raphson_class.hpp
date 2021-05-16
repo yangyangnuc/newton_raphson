2aeb15967c9d1a9f65d0272048ac702c413ffb48
Your Name <you@example.com>
2021-05-16 12:32:36 +0800

    ing

--- src/newton_raphson_class.hpp
+++ src/newton_raphson_class.hpp
@@ -9,10 +9,15 @@ class Newton_raphson
     public:
         double eps;
         unsigned long max_iter;
+        unsigned long iter_time;
 
-        Newton_raphson() : eps(0.001), max_iter(10000)
+        Newton_raphson(double& eps, unsigned long & max_iter) : eps(0.001), max_iter(10000), iter_time(0)
         {
-            ;
+            while (/* condition */)
+            {
+                /* code */
+            }
+            
         } 
 
 }_;
